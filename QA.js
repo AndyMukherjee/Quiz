ab246b0c3b5c8983ef0613512f48afe07e0217a3
AndyMukherjee <andy.mukherjee2005@gmail.com>
2015-04-23 14:56:23 -0400

    Update QA.js

--- QA.js
+++ QA.js
@@ -51,11 +51,12 @@ $('#nxtbtn').click(next);
 
 //----------------------------------------------------
 //disabling the Next button once the last question is reached
-if (Q[currentQ].quizNum === "Question 5"){
+if (Q[currentQ].quizNum = "Question 5"){
 
 	$('#nxtbtn').click(function(){
 		$(this).prop("disabled", true);
-)};
+});
+};
 
 //----------------------------------------------------------
 $('.submit').click(function(){
